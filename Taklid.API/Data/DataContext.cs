72a8ba8ea76b31f2b6efe6d45d3b4f4d464f6940
Developer1 <bdodev1@bdohst.local>
2019-12-25 14:10:24 +0200

    Initial Commit

---
+++ Taklid.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Taklid.API.Models;
+
+namespace Taklid.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
